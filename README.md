e57583ce69b47706f8cb4317f1984eb8d59a2b56
Joo <joo@joostory.net>
2017-03-05 00:56:13 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Comment Sample
+
+Javascript Framework를 테스트하기 위한 샘플 프로젝트
+
+## Getting started
+
+```
+$ npm installl
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  entry: {
+		'react': path.join(__dirname, 'src/react/index'),
+		'vuejs': path.join(__dirname, 'src/vuejs/index')
+	},
+	output: {
+		path: path.join(__dirname, 'docs/assets'),
+    publicPath: '/assets/',
+		filename: '[name].js'
+	},
+	plugins: process.env.NODE_ENV !== "production" ? [] : [
+		new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        drop_console: true
+      }
+    }),
+		new webpack.DefinePlugin({
+		  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+		})
+	],
+	externals: {
+    "jsdom": {}
+  },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: ['babel-loader'],
+				exclude: /node_modules/,
+				include: __dirname
+			}
+		]
+	},
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  }
+}
